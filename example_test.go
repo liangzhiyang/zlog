4007fdfaa37c7ffee6cb43a6d9f785de58bc6959
zhangyangyang <yangyang_zhang@qingsongchou.com>
2017-11-03 18:02:08 +0800

    初始ok版本

---
+++ example_test.go
@@ -0,0 +1,14 @@
+package zlog
+
+//func main() {
+//	// 初始化 日志服务
+//zlog.ChangeConfig(&zlog.Conf{Prefix: "lzy", Dir: "/tmp/log", Mode: zlog.ModeDev})
+//zlog.EnableDebug(true)
+//ctx := zlog.SetTraceInfo(context.Background(), "1", "2", "3")
+//zlog.Info(ctx, "adsfjasdfja;d", "aa", 1)
+//ctx = zlog.With(ctx, "uid", 123, "order_no", "1231231231231231456788")
+//zlog.Debug(ctx, "debug msg")
+//zlog.Info(ctx, "info msg")
+//zlog.Warn(ctx, "warn msg")
+//zlog.Error(ctx, "error msg")
+//}
